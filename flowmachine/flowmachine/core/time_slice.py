897a2ccfbf29b22d633d3ddc74190e992c84e5fb
Maximilian Albert <maximilian.albert@flowminder.org>
2019-04-09 13:38:06 +0100

    Move initialisation from dates into separate TimeSlice.from_dates() method

--- flowmachine/flowmachine/core/time_slice.py
+++ flowmachine/flowmachine/core/time_slice.py
@@ -2,9 +2,19 @@ import datetime as dt
 
 
 class TimeSlice:
-    def __init__(self, *, start_date, end_date):
+    def __init__(self, *, start, stop):
+        assert isinstance(start, str)
+        assert isinstance(stop, str)
+
+        self.start_timestamp = dt.datetime.strptime(start, "%Y-%m-%d %H:%M:%S")
+        self.stop_timestamp = dt.datetime.strptime(stop, "%Y-%m-%d %H:%M:%S")
+        self.start_timestamp_as_str = self.start_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        self.stop_timestamp_as_str = self.stop_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+
+    @classmethod
+    def from_dates(cls, *, start_date, end_date):
         """
-        Initialise time slice from start and end date.
+        Initialise time slice with a start and end date.
 
         The time slice is represented as the half-open time interval [start_timestamp, stop_timestamp),
         where start_timestamp is midnight at the beginning of the the start date and stop_timestamp is
@@ -13,6 +23,9 @@ class TimeSlice:
         For example, if start_date="2016-01-01" and end_date="2016-01-07" then the resulting interval
         is `[2016-01-01 00:00:00, 2016-01-08 00:00:00)`.
 
+        NOTE: during ongoing refactoring this behaviour is actually different so that the end timestamp
+        is actually midnight at the beginning of `end_date`, rather than of the day after `end_date`.
+
         Parameters
         ----------
         start_date : str
@@ -20,15 +33,22 @@ class TimeSlice:
         end_date : str
             ISO format date string for the end of the time slice, e.g. "2016-01-07".
         """
-        ts_start_date = self._parse_date_as_timestamp(start_date)
-        ts_end_date = self._parse_date_as_timestamp(end_date)
+        ts_start_date = cls._parse_date_as_timestamp(start_date)
+        ts_end_date = cls._parse_date_as_timestamp(end_date)
 
-        self.start_timestamp = ts_start_date
-        self.stop_timestamp = ts_end_date
-        # self.stop_timestamp = ts_end_date + dt.timedelta(days=1)
-        self.start_timestamp_as_str = self.start_timestamp.strftime("%Y-%m-%d %H:%M:%S")
-        self.stop_timestamp_as_str = self.stop_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        start_timestamp = ts_start_date
+        stop_timestamp = ts_end_date
+        # stop_timestamp = ts_end_date + dt.timedelta(days=1)  # TODO: re-enable this after refactoring is done
 
-    def _parse_date_as_timestamp(self, date_str):
+        # TODO: once TimeSlice supports initialisation directly from timestamps
+        # we can remove this and pass start_timestamp/stop_timestamp directly to
+        # TimeSlice.__init__()
+        start_str = start_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        stop_str = stop_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+
+        return TimeSlice(start=start_str, stop=stop_str)
+
+    @classmethod
+    def _parse_date_as_timestamp(cls, date_str):
         assert isinstance(date_str, str)
         return dt.datetime.strptime(date_str, "%Y-%m-%d")
--- flowmachine/tests/test_time_slice.py
+++ flowmachine/tests/test_time_slice.py
@@ -11,7 +11,7 @@ from flowmachine.core.time_slice import TimeSlice
 #     """
 #     Start timestamp is midnight at the beginning of start_date and end timestamp is midnight at the end of `end_date`.
 #     """
-#     ts = TimeSlice(start_date="2016-01-01", end_date="2017-09-23")
+#     ts = TimeSlice.from_dates(start_date="2016-01-01", end_date="2017-09-23")
 #     assert ts.start_timestamp == dt.datetime(2016, 1, 1, 0, 0, 0)
 #     assert ts.stop_timestamp == dt.datetime(2017, 9, 24, 0, 0, 0)
 #     assert ts.start_timestamp_as_str == "2016-01-01 00:00:00"
@@ -22,7 +22,7 @@ def test_init_with_start_and_end_dates():
     """
     Start timestamp is midnight at the beginning of start_date and end timestamp is midnight at the beginning of `end_date`.
     """
-    ts = TimeSlice(start_date="2016-01-01", end_date="2017-09-23")
+    ts = TimeSlice.from_dates(start_date="2016-01-01", end_date="2017-09-23")
     assert ts.start_timestamp == dt.datetime(2016, 1, 1, 0, 0, 0)
     assert ts.stop_timestamp == dt.datetime(2017, 9, 23, 0, 0, 0)
     assert ts.start_timestamp_as_str == "2016-01-01 00:00:00"
